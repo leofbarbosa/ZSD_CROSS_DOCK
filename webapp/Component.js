8332c648d179b9ba5457e4e78b7ace6a3d0b8ac8
LEONARDO DA FONSECA BARBOSA <38668909+leofbarbosa@users.noreply.github.com>
2021-04-16 17:39:16 -0400

    Version 1.0
    Initial Development

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("ZSD_CROSS_DOCK.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("ZSD_CROSS_DOCK.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
